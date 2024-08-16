61ecb070c0e1b5583c817c854b694561c82d6582
Andres Munive <fmunive@getontop.com>
2024-08-16 08:58:22 -0500

    New progress cards

--- src/app/modules/trainer/components/progress/progress.component.ts
+++ src/app/modules/trainer/components/progress/progress.component.ts
@@ -14,114 +14,6 @@ export class ProgressComponent implements OnInit {
   currentDay = this.currentDate.getDate();
 
   ngOnInit(): void {}
-  get activesClients(): number {
-    return this.clients?.filter((client) => {
-      const endDate = new Date(client.attributes.endDate);
-      return endDate >= this.currentDate;
-    }).length;
-  }
-  get inactivesClients(): number {
-    return this.clients?.filter((client) => {
-      const endDate = new Date(client.attributes.endDate);
-      return endDate < this.currentDate;
-    }).length;
-  }
-
-  get notPendingClients(): number {
-    return this.clients?.filter((client) => {
-      return client.attributes.hasPaid;
-    }).length;
-  }
-  get pendingClients(): number {
-    return this.clients?.filter((client) => {
-      return !client.attributes.hasPaid;
-    }).length;
-  }
-
-  get threeDaysClients(): number {
-    return this.clients?.filter((client) => {
-      return client.attributes.plan == '3 dias a la semana';
-    }).length;
-  }
-  get sixDaysClients(): number {
-    return this.clients?.filter((client) => {
-      return client.attributes.plan == '6 dias a la semana';
-    }).length;
-  }
-
-  get currentFortnightClients(): number {
-    // Determinar los límites de la quincena actual
-    let startOfFortnight: Date;
-    let endOfFortnight: Date;
-
-    if (this.currentDay <= 15) {
-      // Primera quincena del mes
-      startOfFortnight = new Date(this.currentYear, this.currentMonth, 1);
-      endOfFortnight = new Date(
-        this.currentYear,
-        this.currentMonth,
-        15,
-        23,
-        59,
-        59
-      ); // Final del 15 de mes
-    } else {
-      // Segunda quincena del mes
-      startOfFortnight = new Date(this.currentYear, this.currentMonth, 16);
-      endOfFortnight = new Date(
-        this.currentYear,
-        this.currentMonth + 1,
-        0,
-        23,
-        59,
-        59
-      );
-    }
-
-    return this.clients?.filter((client) => {
-      const startDate = new Date(client.attributes.startDate);
-      return startDate >= startOfFortnight && startDate <= endOfFortnight;
-    }).length;
-  }
-
-  get currentFortnightSixDaysClients(): number {
-    // Determinar los límites de la quincena actual
-    let startOfFortnight: Date;
-    let endOfFortnight: Date;
-
-    if (this.currentDay <= 15) {
-      // Primera quincena del mes
-      startOfFortnight = new Date(this.currentYear, this.currentMonth, 1);
-      endOfFortnight = new Date(
-        this.currentYear,
-        this.currentMonth,
-        15,
-        23,
-        59,
-        59
-      ); // Final del 15 de mes
-    } else {
-      // Segunda quincena del mes
-      startOfFortnight = new Date(this.currentYear, this.currentMonth, 16);
-      endOfFortnight = new Date(
-        this.currentYear,
-        this.currentMonth + 1,
-        0,
-        23,
-        59,
-        59
-      );
-    }
-
-    return this.clients?.filter((client) => {
-      const startDate = new Date(client.attributes.startDate);
-      return (
-        startDate >= startOfFortnight &&
-        startDate <= endOfFortnight &&
-        client.attributes.plan == '6 dias a la semana'
-      );
-    }).length;
-  }
 
   get currentMonthSixDaysClients(): number {
     const currentDate = new Date();
@@ -140,6 +32,7 @@ export class ProgressComponent implements OnInit {
       );
     }).length;
   }
+
   get currentMonthThreeDaysClients(): number {
     const currentDate = new Date();
     const currentYear = currentDate.getFullYear();
@@ -157,6 +50,7 @@ export class ProgressComponent implements OnInit {
       );
     }).length;
   }
+
   get currentMonthThreeDaysClientsPayment(): number {
     const currentDate = new Date();
     const currentYear = currentDate.getFullYear();
@@ -203,12 +97,14 @@ export class ProgressComponent implements OnInit {
     return payment;
   }
 
-  get currentFortnightThreeDaysClients(): number {
-    // Determinar los límites de la quincena actual
+  fortnightClientsPerDays(
+    fortnight: number,
+    plan: string,
+    hasPaid?: boolean
+  ): number {
     let startOfFortnight: Date;
     let endOfFortnight: Date;
-
-    if (this.currentDay <= 15) {
+    if (fortnight == 1) {
       // Primera quincena del mes
       startOfFortnight = new Date(this.currentYear, this.currentMonth, 1);
       endOfFortnight = new Date(
@@ -219,7 +115,7 @@ export class ProgressComponent implements OnInit {
         59,
         59
       ); // Final del 15 de mes
-    } else {
+    } else if (fortnight == 2) {
       // Segunda quincena del mes
       startOfFortnight = new Date(this.currentYear, this.currentMonth, 16);
       endOfFortnight = new Date(
@@ -232,21 +128,36 @@ export class ProgressComponent implements OnInit {
       );
     }
 
+    if (hasPaid == false) {
+      return this.clients?.filter((client) => {
+        const startDate = new Date(client.attributes.startDate);
+        return (
+          startDate >= startOfFortnight &&
+          startDate <= endOfFortnight &&
+          client.attributes.plan == `${plan} dias a la semana` &&
+          client.attributes.hasPaid == false
+        );
+      }).length;
+    }
     return this.clients?.filter((client) => {
       const startDate = new Date(client.attributes.startDate);
       return (
         startDate >= startOfFortnight &&
         startDate <= endOfFortnight &&
-        client.attributes.plan == '3 dias a la semana'
+        client.attributes.plan == `${plan} dias a la semana`
       );
     }).length;
   }
-  get currentFortnightThreeDaysClientsPayment(): number {
-    // Determinar los límites de la quincena actual
+
+  fortnightClientsPaymentPerDays(
+    fortnight: number,
+    plan: string,
+    hasPaid?: boolean
+  ): number {
     let startOfFortnight: Date;
     let endOfFortnight: Date;
 
-    if (this.currentDay <= 15) {
+    if (fortnight == 1) {
       // Primera quincena del mes
       startOfFortnight = new Date(this.currentYear, this.currentMonth, 1);
       endOfFortnight = new Date(
@@ -257,7 +168,7 @@ export class ProgressComponent implements OnInit {
         59,
         59
       ); // Final del 15 de mes
-    } else {
+    } else if (fortnight == 2) {
       // Segunda quincena del mes
       startOfFortnight = new Date(this.currentYear, this.currentMonth, 16);
       endOfFortnight = new Date(
@@ -269,60 +180,28 @@ export class ProgressComponent implements OnInit {
         59
       );
     }
-
-    const filteredClients = this.clients?.filter((client) => {
-      const startDate = new Date(client.attributes.startDate);
-      return (
-        startDate >= startOfFortnight &&
-        startDate <= endOfFortnight &&
-        client.attributes.plan == '3 dias a la semana'
-      );
-    });
-
-    let payment = 0;
-    filteredClients?.forEach((client) => {
-      payment = payment + client.attributes.monthlyPayment;
-    });
-    return payment;
-  }
-
-  get currentFortnightSixDaysClientsPayment(): number {
-    // Determinar los límites de la quincena actual
-    let startOfFortnight: Date;
-    let endOfFortnight: Date;
-
-    if (this.currentDay <= 15) {
-      // Primera quincena del mes
-      startOfFortnight = new Date(this.currentYear, this.currentMonth, 1);
-      endOfFortnight = new Date(
-        this.currentYear,
-        this.currentMonth,
-        15,
-        23,
-        59,
-        59
-      ); // Final del 15 de mes
+    let filteredClients;
+    if (hasPaid == false) {
+      filteredClients = this.clients?.filter((client) => {
+        const startDate = new Date(client.attributes.startDate);
+        return (
+          startDate >= startOfFortnight &&
+          startDate <= endOfFortnight &&
+          client.attributes.plan == `${plan} dias a la semana` &&
+          client.attributes.hasPaid == false
+        );
+      });
     } else {
-      // Segunda quincena del mes
-      startOfFortnight = new Date(this.currentYear, this.currentMonth, 16);
-      endOfFortnight = new Date(
-        this.currentYear,
-        this.currentMonth + 1,
-        0,
-        23,
-        59,
-        59
-      );
+      filteredClients = this.clients?.filter((client) => {
+        const startDate = new Date(client.attributes.startDate);
+        return (
+          startDate >= startOfFortnight &&
+          startDate <= endOfFortnight &&
+          client.attributes.plan == `${plan} dias a la semana`
+        );
+      });
     }
 
-    const filteredClients = this.clients?.filter((client) => {
-      const startDate = new Date(client.attributes.startDate);
-      return (
-        startDate >= startOfFortnight &&
-        startDate <= endOfFortnight &&
-        client.attributes.plan == '6 dias a la semana'
-      );
-    });
     let payment = 0;
     filteredClients?.forEach((client) => {
       payment = payment + client.attributes.monthlyPayment;
@@ -334,7 +213,6 @@ export class ProgressComponent implements OnInit {
     const currentDate = new Date();
     const currentYear = currentDate.getFullYear();
     const currentMonth = currentDate.getMonth(); // Enero es 0, Febrero es 1, etc.
-
     const filteredClients = this.clients?.filter((client) => {
       const startDate = new Date(client?.attributes.startDate);
       const startYear = startDate.getFullYear();
@@ -346,12 +224,15 @@ export class ProgressComponent implements OnInit {
         client.attributes.plan == '6 dias a la semana'
       );
     });
+    console.log(filteredClients);
 
     if (filteredClients?.length > 7) {
       const bonusClients = filteredClients?.slice(7);
       let bonusToPay = 0;
       bonusClients.forEach((client) => {
-        const payment = client.attributes.monthlyPayment - 40000;
+        const payment = client.attributes.hasPaid
+          ? client.attributes.monthlyPayment - 40000
+          : 0;
         bonusToPay = bonusToPay + payment;
       });
       return bonusToPay;
